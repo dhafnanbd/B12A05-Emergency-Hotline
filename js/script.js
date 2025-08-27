3937d08880e980b1403e425c92964aae5d5b5e1f
DH Afnan <dhafnanbd@gmail.com>
2025-08-28 01:44:14 +0600

    Basic Functions Added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log("script.js connected successfully")
+function innerTextAsNumber(id){
+    const rawText = document.getElementById(id).innerText;
+    const convertNumber = parseInt(rawText);
+    return convertNumber;
+}
+
+function setInnerText(id, text){
+    return document.getElementById(id).innerText = text;
+}
+
+const copyButtons = document.querySelectorAll('.btn-copy');
+for (const button of copyButtons){
+    button.addEventListener('click', function(){
+        let currentCopyCount = innerTextAsNumber('total-copy-count');
+        const increaseCopyCount = currentCopyCount+=1;
+        setInnerText('total-copy-count', increaseCopyCount);
+    })
+}
+
+const favButtons = document.querySelectorAll('.fa-heart');
+for (const button of favButtons){
+    button.addEventListener('click', function(){
+        let currentCopyCount = innerTextAsNumber('total-favorite');
+        const increaseCopyCount = currentCopyCount+=1;
+        setInnerText('total-favorite', increaseCopyCount);
+    })
+}
+
+const callButtons = document.querySelectorAll('.btn-call');
+for (const button of callButtons){
+    button.addEventListener('click', function(){
+        let currentCopyCount = innerTextAsNumber('total-coin');
+        const increaseCopyCount = currentCopyCount-=20;
+        setInnerText('total-coin', increaseCopyCount);
+    })
+}
